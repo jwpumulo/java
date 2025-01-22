3ac937d5d57a09ecc7935605c3df49e9cc98d0c1
Jw Pumulo <j.w.pumulo@gmail.com>
2025-01-22 09:00:59 +0900

    0122

--- HelloWorld/src/com/yedam/todo.java
+++ HelloWorld/src/com/yedam/todo.java
@@ -8,13 +8,13 @@ public class todo {
 
 		Scanner scn = new Scanner(System.in); // 시스템에 있는 기본 입력장치
 //ctrl+shift+o
-	
+	int balance = 0; //예금액
 		
 		while (true) {
 			System.out.println("1. 입금 2.출금 3.잔액조회 4.종료");
 		 //num = scn.nextLine(); 
 			
-			System.out.print('메뉴를 선택하세요>")
+			System.out.print('메뉴를 선택하세요>");
 			
 			
 		 int num = Integer.parseInt(scn.nextLine()); // 문자타입반환 > int반환
